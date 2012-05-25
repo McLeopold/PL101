9c1adee82cd034e9c952bcc2fecbe1e4585ff13b
McLeopold <mcleopold@gmail.com>
2012-05-24 17:14:47 -0700

    Add nice list display.
    Previously the check display was outputing lists with no line
    separators.  Add a new function to beautify the parse tree output.

--- web/scheem.js
+++ web/scheem.js
@@ -1,23 +1,38 @@
 // Utility function to log messages
 var log_console = function(msg) {
-  $('#console').append('<p>' + msg + '</p>');
+  $('#console').append('<pre>' + msg + '</pre>');
 };
+var INDENT = '   ';
+var nice_list = function (data, indent) {
+  if (indent === undefined) indent = 2;
+  if (typeof data !== 'object') {
+    return JSON.stringify(data)
+  } else {
+    // assume list
+    return '[\n' +
+           Array(indent).join(INDENT) +
+           data.map(function (data) {
+             return nice_list(data, indent + 1);
+           }).join(',\n' + Array(indent).join(INDENT)) +
+           '\n' + Array(indent-1).join(INDENT) + ']'
+           ;
+  }
+}
 // After page load
 $(function() {
   var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('input'), {
     value: "(begin\n     ;; accumulator maker function\n      (define foo (lambda (n)\n                                  (lambda (i) (begin\n                                                                           (set! n (+ n i))\n                                                                                                       n))))\n      \n       ;; make accumulator starting at 7\n        (define bar (foo 7))\n         \n         (alert (bar 5)) ;; returns 12\n          (alert (bar 3)) ;; return 15\n          )",
     mode:  "scheme",
-    theme: "monokai"
+    //theme: "monokai"
   });
   $('#check').click(function() {
     myCodeMirror.save();
     var user_text = $('#input').val();
     $('#console').html(''); // clear console
-    log_console('Your input was: "' + user_text + '"');
+    //log_console('Your input was: "' + user_text + '"');
     try {
       var parsed = Scheem.parser.parse(user_text);
-      log_console('Parsed: ' + JSON.stringify(parsed));
-      log_console('Program is correct');
+      log_console(nice_list(parsed));
     }
     catch(e) {
       log_console('Parse Error: ' + e);
@@ -27,13 +42,12 @@ $(function() {
     myCodeMirror.save();
     var user_text = $('#input').val();
     $('#console').html(''); // clear console
-    log_console('Your input was: "' + user_text + '"');
+    //log_console('Your input was: "' + user_text + '"');
     try {
       var parsed = Scheem.parser.parse(user_text);
-      log_console('Parsed: ' + JSON.stringify(parsed));
       try {
         var result = Scheem.interpreter.evalScheem(parsed, {});
-        log_console('Result: ' + JSON.stringify(result));
+        log_console(JSON.stringify(result));
       }
       catch(e) {
         log_console('Eval Error: ' + e);
