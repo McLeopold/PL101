24f20876866107c4492cdaba245154696486b658
McLeopold <mcleopold@gmail.com>
2012-05-25 17:17:59 -0700

    Add operators, lists, sets and maps
    - Operators are hard coded in the parser and are infix
    
    - Lists, sets and maps use a [], [,] and [:] syntax.
    
    There are issues with extra parenthesis invoking literals.

--- scheem/scheem_interpreter.js
+++ scheem/scheem_interpreter.js
@@ -36,48 +36,77 @@ Scheem.interpreter = (function () {
       } else {
         update(env.outer, v, val);
       }
+      return val;
     } else {
       throw new Error('variable ' + v + ' not defined');
     }
   };
-
-  var add_env = function (env) {
-    env.outer = {bindings: env.bindings,
-                 outer: env.outer};
-    env.bindings = {};
-  }
 
   var add_binding = function (env, v, val) {
     if (!env.bindings) {
       env.bindings = {};
       env.outer = {};
     }
-    env.bindings[v] = val;
+    return env.bindings[v] = val;
   };
 
   var initial_env = {
     'assert-args-0': function () {
-      return arguments.length === 0 ? '#t' : '#f';
+      return arguments.length === 0 ? true : false;
     },
     'assert-args-1': function () {
-      return arguments.length === 1 ? '#t' : '#f';
+      return arguments.length === 1 ? true : false;
     },
     'assert-args-2': function () {
-      return arguments.length === 2 ? '#t' : '#f';
+      return arguments.length === 2 ? true : false;
     },
-    '+': function (x, y) { return x + y; },
-    '-': function (x, y) { return x - y; },
+    // operators
+    // access
+    '.': function (x, y) {
+      return x.y;
+    },
+    // unary
+    '~': function (x) { return ~x; },
+    // exponent
+    '^': function (x, y) { return Math.pow(x, y); },
+    // multiplication
     '*': function (x, y) { return x * y; },
     '/': function (x, y) { return x / y; },
-    '==': function (x, y) { return x === y ? '#t' : '#f'; },
-    '>': function (x, y) { return x > y ? '#t' : '#f'; },
-    '<': function (x, y) { return x < y ? '#t' : '#f'; },
+    '//': function (x, y) { return Math.floor(x / y); },
+    '%': function (x, y) { return x % y; },
+    // addition / subtraction
+    '+': function (x, y) { return x + y; },
+    '++': function (x, y) { return x + y; },
+    '-': function (x, y) { return x - y; },
+    // bitshift
+    '>>': function (x, y) { return x >> y; },
+    '<<': function (x, y) { return x << y; },
+    // bitwise
+    '&': function (x, y) { return x & y; },
+    '|': function (x, y) { return x | y; },
+    '><': function (x, y) { return x ^ y; },
+    // comparison
+    '==': function (x, y) { return x === y ? true : false; },
+    '!=': function (x, y) { return x !== y ? true : false; },
+    '>': function (x, y) { return x > y ? true : false; },
+    '<': function (x, y) { return x < y ? true : false; },
+    '>=': function (x, y) { return x >= y ? true : false; },
+    '<=': function (x, y) { return x <= y ? true : false; },
     '<=>': function (x, y) { return x < y ? -1 : (x === y ? 0 : 1); },
+    'in': function (x, y) { return y.indexOf(x) >= 0 ? true : false; },
+    // logical
+    'not': function (x, y) { return x ^ y; },
+    'and': function (x, y) { return x & y; },
+    'or': function (x, y) { return x | y; },
+    'xor': function (x, y) { return x ^ y; },
+    // builtins
     'cons': function (x, y) { return [x].concat(y); },
     'car': function (x, y) { return x[0]; },
     'cdr': function (x, y) { x.shift(); return x; },
     'cdr': function (x, y) { return x.slice(1); },
-    'alert': function (arg) { console.log(arg); return arg; }
+    'alert': function (arg) {
+      console.log(arg); return arg;
+    }
   };
 
   var evalScheem = function (expr, env) {
@@ -91,63 +120,86 @@ Scheem.interpreter = (function () {
     }
     // Look at head of list for operation
     switch (expr[0]) {
-      case '=':
-        if (env === null) {
-          env = {name: expr[1],
-                 value: evalScheem(expr[2]),
-                 outer: null};
-        } else if (env === {}) {
-          env.name = expr[1];
-          env.value = evalScheem(expr[2]);
-          env.outer = null;
-        } else {
-          add_binding(env, expr[1], evalScheem(expr[2], env));
-        }
-        //env[expr[1]] = evalScheem(expr[2], env);
-        return 0;
-      case 'set!':
-        update(env, expr[1], evalScheem(expr[2], env));
-        //if (!env[expr[1]]) throw new Error("set! variable " + expr[1] + " not defined");
-        //env[expr[1]] = evalScheem(expr[2], env);
-        return 0;
-      case 'begin':
+      case 'do':
         var result = 0;
         for (var i = 1; i < expr.length; ++i) {
           result = evalScheem(expr[i], env);
         }
-        return result;            
+        return result;
+      case 'list':
+        var result = [];
+        for (var i = 0, ilen = expr[1].length; i < ilen; ++i) {
+          result.push(evalScheem(expr[1][i], env));
+        }
+        return result;
+      case 'set':
+        var result = {};
+        for (var i = 0, ilen = expr[1].length; i < ilen; ++i) {
+          result[evalScheem(expr[1][i], env)] = true;
+        }
+        return result;
+      case 'map':
+        var result = {};
+        for (var i = 0, ilen = expr[1].length; i < ilen; ++i) {
+          result[evalScheem(expr[1][i][0], env)] = evalScheem(expr[1][i][1], env);
+        }
+        return result;
       case 'quote':
         if (expr.length !== 2) throw new Error("quote expects 1 parameter, got " + (expr.length - 1));
         return expr[1];
       case 'if':
         var t = evalScheem(expr[1], env);
-        if (t === '#t') {
+        if (t === true) {
           return evalScheem(expr[2], env);
         } else {
           return evalScheem(expr[3], env);
         }
       case 'fn':
         return function () {
+          env = {bindings: {}, outer: env};
           for (var i = 0, ilen = expr[1].length; i < ilen; ++i) {
             add_binding(env, expr[1][i], arguments[i]);
           }
           return evalScheem(expr[2], env);
         };
+      case 'scope':
+        return evalScheem(expr[1], {bindings: {}, outer: env});
       case 'let':
         for (var i = 0, ilen = expr[1].length; i < ilen; ++i) {
           add_binding(env, expr[1][i][0], evalScheem(expr[1][i][1]));
         }
         return evalScheem(expr[2], env);
-      case 'let-one':
-        add_binding(env, expr[1], evalScheem(expr[2]));
-        return evalScheem(expr[3], env);
+      case '.':
+        return evalScheem(expr[1])[expr[2]];
+      case 'define':
+      case ':=':
+        return add_binding(env, expr[1], evalScheem(expr[2], env));
+      case 'set!':
+      case '=':
+        return update(env, expr[1], evalScheem(expr[2], env));
+      case '^=':
+      case '*=':
+      case '/=':
+      case '%=':
+      case '//=':
+      case '+=':
+      case '++=':
+      case '-=':
+      case '<<=':
+      case '>>=':
+      case '&=':
+      case '|=':
+      case '><=':
+        var func = evalScheem(expr[0].slice(0, -1), env);
+        var val = func.call(env, evalScheem(expr[1], env), evalScheem(expr[2], env))
+        return update(env, expr[1], val);
       default:
         var func = evalScheem(expr[0], env);
         var args = [];
         for (var i = 1, ilen = expr.length; i < ilen; ++i) {
           args.push(evalScheem(expr[i], env));
         }
-        return func.apply(null, args);
+        return func.apply(env, args);
     }
   };
 
--- web/scheem.js
+++ web/scheem.js
@@ -5,7 +5,7 @@ var log_console = function(msg) {
 var INDENT = '   ';
 var nice_list = function (data, indent) {
   if (indent === undefined) indent = 2;
-  if (typeof data !== 'object') {
+  if (!(data instanceof Array)) {
     return JSON.stringify(data)
   } else {
     // assume list
@@ -17,6 +17,16 @@ var nice_list = function (data, indent) {
            '\n' + Array(indent-1).join(INDENT) + ']'
            ;
   }
+}
+var nice_env = function (data) {
+  for (var prop in data) {
+    if (typeof data[prop] === 'function') {
+      data[prop] = 'function';
+    } else if (typeof data[prop] === 'object') {
+      nice_env(data[prop]);
+    }
+  }
+  return data;
 }
 // After page load
 $(function() {
@@ -45,8 +55,10 @@ $(function() {
     try {
       var parsed = Scheem.parser.parse(user_text);
       try {
-        var result = Scheem.interpreter.evalScheem(parsed, {});
+        var env = {};
+        var result = Scheem.interpreter.evalScheem(parsed, env);
         log_console(JSON.stringify(result));
+        log_console(JSON.stringify(nice_env(env)));
       }
       catch(e) {
         log_console('Eval Error: ' + e);
